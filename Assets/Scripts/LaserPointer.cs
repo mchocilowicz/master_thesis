78b062a003c3b82bd651f9b074c555834958ff5d
mchocilowicz <marcin.chocilowicz@gmail.com>
2017-03-16 22:32:03 +0100

    Player can now change location by teleportation

---
+++ Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    public GameObject laserPrefab;
+
+    private GameObject laser;
+
+    private Transform laserTransform;
+
+    private Vector3 hitPoint;
+
+    public Transform cameraRigTransform;
+    public GameObject teleportMarkerPrefab;
+    private GameObject marker;
+    private Transform teleportMarkerTransform;
+    public Transform headTransform;
+    public Vector3 teleportMarkerOffset;
+    public LayerMask teleportMask;
+    private bool shouldTeleport;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private void ShowLaser (RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+    }
+
+    private void Teleport()
+    {
+        shouldTeleport = false;
+        marker.SetActive(false);
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        difference.y = 0;
+        cameraRigTransform.position = hitPoint + difference;
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+        marker = Instantiate(teleportMarkerPrefab);
+        teleportMarkerTransform = marker.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+
+            if( Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100, teleportMask))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+                marker.SetActive(true);
+                teleportMarkerTransform.position = hitPoint + teleportMarkerOffset;
+                shouldTeleport = true;
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+            marker.SetActive(false);
+        }
+
+        if(Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad) && shouldTeleport)
+        {
+            Teleport();
+        }
+	}
+}
