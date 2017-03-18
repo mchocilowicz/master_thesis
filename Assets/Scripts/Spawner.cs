e8490b11808cbc6767e7d1deb7ef0c2cb4ff3f57
mchocilowicz <marcin.chocilowicz@gmail.com>
2017-03-18 16:08:30 +0100

    Spawning Enemies

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public int numberOfEnemies;
+    public float spawnRadius;
+    public GameObject objectToSpawn;
+    private Vector3 spawnPosition;
+
+	// Use this for initialization
+	void Start () {
+        SpawnObject();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnObject ()
+    {
+        for(int i = 0; i < numberOfEnemies; i++)
+        {
+            spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
+            Vector3 adjustedtPosition = new Vector3(spawnPosition.x, 0, spawnPosition.z);
+            Instantiate(objectToSpawn, adjustedtPosition, Quaternion.identity);
+        }    
+    }
+}
