fa94ae09ecec4f7ffa2fb440f3900c0db06dc0c8
mchocilowicz <djsz-29@sr.dmcs.pl>
2017-04-08 20:34:23 +0200

    Added Fade In/Out when teleporting

---
+++ Assets/Scripts/TeleportPad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPad : MonoBehaviour {
+
+    public Vector3 teleportTo;
+    public Transform cameraRig;
+
+	IEnumerator OnTriggerEnter(Collider other)
+    {
+		if (other.gameObject.name == "Player") {
+			Fade fade = GameObject.FindGameObjectWithTag ("Fade").GetComponent<Fade> ();
+			yield return StartCoroutine (fade.FadeToBlack ());
+			cameraRig.position = teleportTo;
+			yield return StartCoroutine(fade.FadeToClear());
+		}
+    }
+
+}
+
